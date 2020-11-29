3a111f504e5c64319ed0ef250f2e564525e940cb
jose <joseluis19963@gmail.com>
2020-11-29 16:09:51 -0500

    web page search engine

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -17,12 +17,33 @@ class Articles extends Component {
 
     componentWillMount() {
         var home = this.props.home;
+        var search = this.props.search;
 
         if (home === 'true') {
             this.getLastArticles();
+        } else if (search && search !== null && search !== undefined) {
+            this.getArticlesBySearch(search);
         } else {
             this.getArticles();
         }
+    }
+
+    getArticlesBySearch = (searched) => {
+        axios.get(this.url + "search/" + searched)
+            .then(res => {
+
+                this.setState({
+                    articles: res.data.articles,
+                    status: 'success'
+                });
+
+            })
+            .catch(err => {
+                this.setState({
+                    articles: [],
+                    status: 'success'
+                });
+            });
     }
 
     getLastArticles = () => {
@@ -32,7 +53,6 @@ class Articles extends Component {
                     articles: res.data.articles,
                     status: 'success'
                 });
-                console.log(this.state);
             });
     }
 
@@ -51,7 +71,7 @@ class Articles extends Component {
 
             var listArticles = this.state.articles.map((article) => {
                 return (
-                    <article className="article-item" id="article-template">
+                    <article className="article-item" id="article-template" key={article._id}>
                         <div className="image-wrap">
                             {article.image != null
                                 ? (<img src={this.url + 'get-image/' + article.image} alt={article.title} />)
